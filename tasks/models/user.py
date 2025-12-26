9774becd068f567a87f860378e41657f271069cd
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-26 23:08:36 +0545

    changing model for user

--- tasks/models/user.py
+++ tasks/models/user.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    bio = models.TextField(blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
