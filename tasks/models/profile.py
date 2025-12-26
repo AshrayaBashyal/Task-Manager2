a85f18bfba9a7bd90d7862180249c01c14def08f
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-26 23:12:17 +0545

    model for profile

---
+++ tasks/models/profile.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    avatar = models.URLField(blank=True)
+    website = models.URLField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
