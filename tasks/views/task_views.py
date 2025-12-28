bad69901599c6ddfe9a3dd68cc42a8b3d8707d25
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-28 14:34:11 +0545

    add task views

---
+++ tasks/views/task_views.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from ..models.task import Task
+from ..searializers.task_serializers import TaskSerializer
+from ..permissions.task_permissions import IsTaskOwner
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # First security layer: users only see their own tasks
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        # Prevent users from assigning tasks to other users
+        serializer.save(user=self.request.user)
+
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsTaskOwner]
+    queryset = Task.objects.all()   # Needed for object-permission checking
+
+    def get_queryset(self):
+        # Second security layer: limit DB queries to user-owned tasks
+        return Task.objects.filter(user=self.request.user)
