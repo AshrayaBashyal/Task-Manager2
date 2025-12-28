d5eb17b26e30cb7d29c9dc0b532afb2f37408c6f
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-28 15:54:33 +0545

    setting up serializers init file

---
+++ tasks/searializers/__init__.py
@@ -0,0 +1,13 @@
+# tasks/serializers/__init__.py
+from .task_serializers import TaskSerializer
+from .user_serializers import UserSerializer, ProfileSerializer
+from .auth_serializers import RegisterSerializer, LoginSerializer, LogoutSerializer
+
+__all__ = [
+    "TaskSerializer",
+    "UserSerializer",
+    "ProfileSerializer",
+    "RegisterSerializer",
+    "LoginSerializer",
+    "LogoutSerializer",
+]
