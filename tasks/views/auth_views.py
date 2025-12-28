e403ceff2976fc268913ca1eec08dbaf6ed8950a
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-28 17:43:39 +0545

    fixing swagger ui's register hiding problem

--- tasks/views/auth_views.py
+++ tasks/views/auth_views.py
@@ -5,13 +5,19 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from ..searializers.auth_serializers import RegisterSerializer, LoginSerializer, LogoutSerializer
 from ..searializers.user_serializers import ProfileSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 
 class RegisterView(GenericAPIView):
-    serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
 
-def post(self, request):
+    @swagger_auto_schema(
+        security=[],   # tells swagger: NO auth required
+        request_body=RegisterSerializer
+    )
+
+    def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
@@ -19,8 +25,10 @@ def post(self, request):
 
 
 class LoginView(GenericAPIView):
-    serializer_class = LoginSerializer
     permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+
+    @swagger_auto_schema(security=[], request_body=LoginSerializer)
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
