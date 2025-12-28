074d96dba78eaf4b2b59bc73d8a9418058698458
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-28 14:54:36 +0545

    setting up urls

--- config/settings.py
+++ config/settings.py
@@ -49,10 +49,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
 
-    # Dev tools
     'drf_yasg',
-    'django_filters',
-    'django_extensions',
 
     'tasks',
 ]
